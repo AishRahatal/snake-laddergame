98ddf5f49b9ba84cf0fc2cbe6416710a57dbd396
aishrahatal <89258379+aishrahatal@users.noreply.github.com>
2021-09-19 13:57:53 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@ using System.Threading;
 namespace snake_ladder
 {
     class Program
-    {
+     {
+          // UC1 for single player
 
 
-
- public static int Main(String[] args)
+       public static int Main(String[] args)
    {
 
             board b = new board(); 
@@ -25,22 +25,25 @@ namespace snake_ladder
 
             Console.WriteLine( "***********************Dice score***********************\n");
             int score;
-            while (true)
+            int count =0;
+            while (true) // UC4- Repeat till the Player reaches the winning position 100.
             {
                 // player part.
                 player1.previousposition = player1.currentposition;
                 score = player1.Rolling_dice();   // calling Rolling_dice function. 
-
+                 count++;
                 player1.currentposition = b.nextposition(score, player1.previousposition);
                 if (player1.currentposition > 100)
                    player1.currentposition = player1.previousposition;
                 
+                // UC6-Report the number of times the dice was played to win the game and also the position after every die role
+                Console.WriteLine(" Player1 : "+player1.name + "\t||  No of time dice roll :"+count);
                 Console.WriteLine("Previous Position :\t" + player1.previousposition);
-                Console.WriteLine(  "\nDice score     :\t"+score  );
+                Console.WriteLine(  "\nDice score     :\t"+score  ); 
                 Console.WriteLine("\nCurrent Position :\t" + player1.currentposition);
                 Console.WriteLine("------------------------------------------------------------------------");
 
-                if (player1.currentposition == 100)
+                if (player1.currentposition == 100) //UC5- Ensure the player gets to exact winning position 100.
                 {
                     Console.WriteLine(" Player won the Game "  );
                     break;
